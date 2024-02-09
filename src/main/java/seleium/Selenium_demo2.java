d2ffc4c2eed90a92474fffc400e85a0a2ee1566f
Admin <Admin@DESKTOP-1L1HD6G>
2024-02-09 20:32:43 +0530

    commit

---
+++ src/main/java/seleium/Selenium_demo2.java
@@ -0,0 +1,63 @@
+package seleium;
+
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Selenium_demo2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+        System.setProperty("WebDriver.chrome.driver","chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+//		driver.findElement(By.id("name")).sendKeys("vinod");
+
+//	    driver.findElement(By.id("email")).sendKeys("vinodthumati116@gmail.com");
+	    
+//		driver.findElement(By.id("phone")).sendKeys("912345678");
+		
+//		
+//		driver.findElement(By.id("textarea")).sendKeys("hyderabad");
+		
+//		WebElement checkbox=driver.findElement(By.id("male"));
+//        checkbox.click();
+//        System.out.println(checkbox.isSelected());
+//        
+//        WebElement checkboxs=driver.findElement(By.id("friday"));
+//        checkboxs.click();
+//        System.out.println(checkboxs.isSelected());
+//        
+//		 List<WebElement> dropdown= driver.findElements(By.xpath("//select[@id='country']"));
+//	        for(WebElement options:dropdown) {
+//	        	driver.findElement(By.id("country")).sendKeys("India");
+//	        	options.click();
+//	        	}    
+	        
+//	        driver.findElement(By.id("datepicker")).sendKeys("12/02/2024");
+		
+//		List<WebElement> colors= driver.findElements(By.xpath("//select[@id='colors']"));
+//        for(WebElement options:colors) {
+//        	//driver.findElement(By.id("colors")).sendKeys("Blue");
+//        	options.click();
+//        	}    
+	        
+//		WebElement button=driver.findElement(By.xpath("//button[text()=' New Browser Window']"));
+//		System.out.println("verifying the button" +button.isEnabled() );
+
+		driver.findElement(By.xpath("//button[text()=' Alert']")).click();
+
+		Alert al=driver.switchTo().alert();
+		al.accept();
+
+
+	}
+
+}
--- src/main/java/seleium/Seleniumwebelements_demo.java
+++ src/main/java/seleium/Seleniumwebelements_demo.java
@@ -19,36 +19,36 @@ public class Seleniumwebelements_demo {
 		
 		driver.get("https://trytestingthis.netlify.app/");
 		
-		driver.findElement(By.id("fname")).sendKeys("vinod");
-		
-		driver.findElement(By.id("lname")).sendKeys("thumati");
-        
-		WebElement checkbox=driver.findElement(By.id("male"));
-        checkbox.click();
-        System.out.println(checkbox.isSelected());
-        
-        WebElement text=driver.findElement(By.xpath("//h1[text()='Your Website to practice Automation Testing']"));
-        System.out.println("the text is displayed "+text.isDisplayed());
-        
+//		driver.findElement(By.id("fname")).sendKeys("vinod");
+//		
+//		driver.findElement(By.id("lname")).sendKeys("thumati");
+//        
+//		WebElement checkbox=driver.findElement(By.id("male"));
+//        checkbox.click();
+//        System.out.println(checkbox.isSelected());
+//        
+//        WebElement text=driver.findElement(By.xpath("//h1[text()='Your Website to practice Automation Testing']"));
+//        System.out.println("the text is displayed "+text.isDisplayed());
+//        
         WebElement button=driver.findElement(By.xpath("//button[text()=' Submit']"));
         System.out.println("verifying the button "+button.isEnabled());
       
        // WebElement option=driver.findElement(By.xpath("//label[text()='Option 2']"));
         //System.out.println(option.isSelected());
       
-        List<WebElement> tags= driver.findElements(By.tagName("div"));
-        System.out.println(tags.size());
-       
-        List<WebElement> checks= driver.findElements(By.xpath("//input[@id='moption']"));
-        for(WebElement options:checks) {
-        	options.click();
-        	}    
-        driver.findElement(By.tagName("textarea")).clear();
-		driver.findElement(By.tagName("textarea")).sendKeys(" welcome to selenium");
-
-
-        WebElement buttons=driver.findElement(By.xpath("//button[text()=' Submit']"));
-        System.out.println("verifying the submit button "+button.isEnabled());
+//        List<WebElement> tags= driver.findElements(By.tagName("div"));
+//        System.out.println(tags.size());
+//       
+//        List<WebElement> checks= driver.findElements(By.xpath("//input[@id='moption']"));
+//        for(WebElement options:checks) {
+//        	options.click();
+//        	}    
+//        driver.findElement(By.tagName("textarea")).clear();
+//		driver.findElement(By.tagName("textarea")).sendKeys(" welcome to selenium");
+//
+//
+//        WebElement buttons=driver.findElement(By.xpath("//button[text()=' Submit']"));
+//        System.out.println("verifying the submit button "+button.isEnabled());
 
 	}
 
